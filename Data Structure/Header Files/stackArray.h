70ec70e988b8196970a1206cfc4b726be9961efb
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2025-02-18 23:52:07 +0530

    Update stackArray.h

--- Data Structure/Header Files/stackArray.h
+++ Data Structure/Header Files/stackArray.h
@@ -2,25 +2,93 @@
 #ifndef STACKARRAY_H
 #define STACKARRAY_H
 
+#include <iostream>
+#include <cstring>  // For memcpy
+#include <climits>  // For INT_MIN
+
+template<typename T>
 class Stack {
 private:
-    int* array;
+    T* array;  // Dynamic array to store elements
     int top;
     int capacity;
 
 public:
-    Stack(int cap) : capacity(cap), top(-1), array(new int[cap]) {}
+    Stack(int cap) : capacity(cap), top(-1), array(new T[cap]) {}
 
-    static Stack* createStack(int capacity);
-    int isEmpty(Stack* S) const;
-    int Size(Stack* S) const;
-    int isFull(Stack* S) const;
-    void Push(Stack* S, int data);
-    void doubleStack(Stack* S);
-    void PushIntoDoubleStack(Stack* S, int data);
-    int Pop(Stack* S);
-    int Peek(Stack* S) const;
-    void deleteStack(Stack* S);
+    ~Stack() {
+        delete[] array;  // Free allocated memory
+    }
+
+    void Push(T data);
+    T Pop();
+    T Peek() const;
+    bool isEmpty() const;
+    bool isFull() const;
+    int Size() const;
+    void doubleStack();
+    void deleteStack();
 };
+
+// Implementation of template methods must be in the header file
+template<typename T>
+void Stack<T>::Push(T data) {
+    if (isFull()) {
+        doubleStack();
+    }
+    array[++top] = data;
+}
+
+template<typename T>
+T Stack<T>::Pop() {
+    if (isEmpty()) {
+        std::cerr << "Stack is Empty\n";
+        return T();  // Return default value of T
+    }
+    return array[top--];
+}
+
+template<typename T>
+T Stack<T>::Peek() const {
+    if (isEmpty()) {
+        std::cerr << "Stack is Empty\n";
+        return T();  // Return default value of T
+    }
+    return array[top];
+}
+
+template<typename T>
+bool Stack<T>::isEmpty() const {
+    return (top == -1);
+}
+
+template<typename T>
+bool Stack<T>::isFull() const {
+    return (top == capacity - 1);
+}
+
+template<typename T>
+int Stack<T>::Size() const {
+    return (top + 1);
+}
+
+template<typename T>
+void Stack<T>::doubleStack() {
+    capacity *= 2;
+    T* newArray = new T[capacity];
+
+    std::memcpy(newArray, array, (top + 1) * sizeof(T));
+
+    delete[] array;
+    array = newArray;
+}
+
+template<typename T>
+void Stack<T>::deleteStack() {
+    delete[] array;
+    array = nullptr;
+    top = -1;
+    capacity = 0;
+}
 
 #endif
