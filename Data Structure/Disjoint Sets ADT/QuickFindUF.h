fb97fd9e0e33402be8b898886d97b43d28870413
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2025-03-06 16:00:48 +0530

    Create QuickFindUF.h

---
+++ Data Structure/Disjoint Sets ADT/QuickFindUF.h
@@ -0,0 +1,15 @@
+class QuickFindUF {
+private:
+    std::vector<int> id;
+public:
+    QuickFindUF(int n) {
+        id.resize(n);
+        for (int i = 0; i < n; i++) id[i] = i;
+    }
+    int Find(int x) { return id[x]; }
+    void Union(int x, int y) {
+        int oldID = id[y], newID = id[x];
+        for (int i = 0; i < id.size(); i++)
+            if (id[i] == oldID) id[i] = newID;
+    }
+};
