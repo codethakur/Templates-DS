309f583cb0a1db5939c9a1737233d3ebe3075408
KISHAN THAKUR <83303141+codethakur@users.noreply.github.com>
2025-05-29 18:40:32 +0530

    Update Vector.h
    ITERATOR

--- Data Structure/Vector/Vector.h
+++ Data Structure/Vector/Vector.h
@@ -1,64 +1,83 @@
 #pragma once
-#include <cstddef>    // size_t
-#include <utility>    // std::move, std::forward
+#include <cstddef> // size_t
+#include <utility> // std::move, std::forward
+#include "Iterator.h"
 
 template <typename T, size_t InitialCapacity = 2>
-class Vector {
+class Vector
+{
 public:
-    Vector() : m_Data(nullptr), m_Size(0), m_Capacity(0) {
+    using value_type = T;  
+    using iterator = vectorIterator<Vector<T>>;
+
+
+    Vector() : m_Data(nullptr), m_Size(0), m_Capacity(0)
+    {
         ReAlloc(InitialCapacity);
     }
 
-    ~Vector() {
+    ~Vector()
+    {
         clear();
         ::operator delete(m_Data);
-
     }
 
-    void PushBack(const T& value) {
+    void PushBack(const T &value)
+    {
         if (m_Size >= m_Capacity)
             ReAlloc(m_Capacity + m_Capacity / 2);
         new (&m_Data[m_Size++]) T(value);
     }
 
-    void PushBack(T&& value) {
+    void PushBack(T &&value)
+    {
         if (m_Size >= m_Capacity)
             ReAlloc(m_Capacity + m_Capacity / 2);
         new (&m_Data[m_Size++]) T(std::move(value));
     }
 
-    template<typename... Args>
-    void EmplaceBack(Args&&... args) {
+    template <typename... Args>
+    void EmplaceBack(Args &&...args)
+    {
         if (m_Size >= m_Capacity)
             ReAlloc(m_Capacity + m_Capacity / 2);
         new (&m_Data[m_Size++]) T(std::forward<Args>(args)...);
     }
 
-    void PopBack() {
+    void PopBack()
+    {
         if (m_Size > 0)
             m_Data[--m_Size].~T();
     }
 
-    void clear() {
+    void clear()
+    {
         for (size_t i = 0; i < m_Size; ++i)
             m_Data[i].~T();
         m_Size = 0;
     }
 
-    T& operator[](size_t index) { return m_Data[index]; }
-    const T& operator[](size_t index) const { return m_Data[index]; }
+    T &operator[](size_t index) { return m_Data[index]; }
+    const T &operator[](size_t index) const { return m_Data[index]; }
 
     size_t Size() const { return m_Size; }
-
-    void ShrinkToFit() {
+    void ShrinkToFit()
+    {
         if (m_Size < m_Capacity)
             ReAlloc(m_Size);
     }
 
+    iterator begin()noexcept { return iterator(m_Data); }
+    iterator end() noexcept { return iterator(m_Data + m_Size);}
+    
+    iterator begin() const{ return iterator(m_Data); }
+    iterator end() const { return iterator(m_Data + m_Size);}
+
 
 private:
-    void ReAlloc(size_t newCapacity) {
-        T* newBlock = static_cast<T*>(::operator new(newCapacity * sizeof(T)));
+    void ReAlloc(size_t newCapacity)
+    {
+        T *newBlock = static_cast<T *>(::operator new(newCapacity * sizeof(T)));
 
         for (size_t i = 0; i < m_Size; ++i)
             new (&newBlock[i]) T(std::move(m_Data[i]));
@@ -72,7 +91,7 @@ private:
     }
 
 private:
-    T* m_Data = nullptr;
+    T *m_Data;
     size_t m_Size;
     size_t m_Capacity;
 };
